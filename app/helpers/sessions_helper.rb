be6b7cca50247ef9f26750a271e156d0f97b0f50
Katsuya Tajima <zakichan1216@gmail.com>
2017-03-18 15:32:09 +0900

    10.2.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,23 @@
 module SessionsHelper
+
+  # 渡されたユーザーをログイン
   def log_in(user)
     session[:user_id] = user.id
   end
 
+  # 永続セッションとしてユーザーを記憶する
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
+  # 記憶トークン (cookie) に対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
