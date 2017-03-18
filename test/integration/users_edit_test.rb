d7c1050dbfd2216482c274b1f45cb9c3f92b83f0
Katsuya Tajima <zakichan1216@gmail.com>
2017-03-18 16:32:33 +0900

    10.3.1

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test "unsuccessful edit" do
     log_in_as(@user)
     get edit_user_path(@user)
